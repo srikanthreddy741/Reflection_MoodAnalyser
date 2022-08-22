a111ea2da0027d50c4ed19e1f5ac0a75476884a6
srikanthreddy741 <bandarisrikanth741@gmail.com>
2022-08-22 22:19:48 +0530

    Mood Analyser with default constructor

--- ReflectionMoodAnalyser/TestProject1/UnitTest1.cs
+++ ReflectionMoodAnalyser/TestProject1/UnitTest1.cs
@@ -11,7 +11,10 @@ namespace MoodAnalyserTest
         {
             moodAnalyserfactory = new MoodAnalyseFactory();
         }
+
+        /// <summary>
         /// TC-4.1 Given MoodAnalyse Class Name Should Return MoodAnalyser Object
+        /// </summary>
         [Test]
         public void MoodAnalyserClassName_ShouldReturn_MoodAnalyserObject()
         {
@@ -20,7 +23,10 @@ namespace MoodAnalyserTest
             object obj = MoodAnalyseFactory.CreateMoodAnalyse("ReflectionMoodAnalyser.MoodAnalyser", "MoodAnalyser");
             expected.Equals(obj);
         }
+
+        /// <summary>
         /// TC-4.2 Given MoodAnalyse Class Name When Improper Should Throw Exception
+        /// </summary>,
         [Test]
         public void MoodAnalyserClassName_Improper_Should_ThrowMoodAnalyserException()
         {
@@ -36,7 +42,10 @@ namespace MoodAnalyserTest
                 Assert.AreEqual(expected, exception.Message);
             }
         }
+
+        /// <summary>
         /// TC-4.3 Given MoodAnalyse Class Name When Constructor is Improper Should Throw Exception
+        /// </summary >
         [Test]
         public void MoodAnalyserClassName_ConstructorIsImproper_Should_ThrowMoodAnalyserException()
         {
